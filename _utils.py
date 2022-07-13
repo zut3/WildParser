eb2584415a886509e3eb5254f33d74a83fb3e417
глеб зайцев <z.gl3b@yandex.ru>
2022-07-13 11:38:53 +0300

    refactoring and added req.txt

--- _utils.py
+++ _utils.py
@@ -2,11 +2,6 @@ import aiohttp
 import json
 from typing import Optional
 
-# template_products_url = "https://search.wb.ru/exactmatch/ru/common/v4/" \
-#                             "search?appType=1&curr=rub" \
-#                             "&emp=0" \
-#                             "$lang=ru&locale=ru&page={page}&pricemarginCoeff=1.0" \
-#                             "&query={query}&reg=0&resultset=catalog&sort=popular&spp=0"
 
 template_products_url = "https://search.wb.ru/exactmatch/ru/common/v4/search?" \
                         "appType=1&couponsGeo=2,12,7,3,6,18,21&curr=rub&dest=-1029256,-81994,-912503,-941037&emp=0&lang=ru&locale=ru&page={page}&pricemarginCoeff=1.0&query={query}&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0"
@@ -15,9 +10,11 @@ template_products_url = "https://search.wb.ru/exactmatch/ru/common/v4/search?" \
 template_product_description_url = "https://wbx-content-v2.wbstatic.net/ru/{idx}.json"
 
 
-def get_image_url(index: str) -> str:
+def get_image_url(index: str, photo_count: int | None) -> list:
     template_images_url = "https://images.wbstatic.net/c516x688/new/{collection}/{idx}.jpg"
-    return template_images_url.format(collection=str(index)[:4] + "0000", idx=str(index) + "-1")
+
+    return [template_images_url.format(collection=str(index)[:4] + "0000", idx=str(index) + f"-{i}")
+            for i in range(1, photo_count or 2)]
 
 
 def normalize_text(text: str | None) -> Optional[str]:
@@ -28,7 +25,6 @@ def normalize_text(text: str | None) -> Optional[str]:
 
 async def get_products(query: str, page: int, session: aiohttp.ClientSession) -> dict:
     url = template_products_url.format(query=query, page=page)
-    print(url)
     async with session.get(url) as response:
         text = await response.read()
         return json.loads(text)
--- async_parser.py
+++ async_parser.py
@@ -1,29 +1,12 @@
 import asyncio
 import aiohttp
 import time
-import json
 import _utils
 from _utils import normalize_text as _
-import aiofiles
 from fake_useragent import UserAgent
 
 
-# template_products_url = "https://search.wb.ru/exactmatch/ru/common/v4/" \
-#                             "search?appType=1&curr=rub" \
-#                             "&emp=0" \
-#                             "$lang=ru&locale=ru&page={page}&pricemarginCoeff=1.0" \
-#                             "&query={query}&reg=0&resultset=catalog&sort=popular&spp=0"
-
-template_products_url = "https://search.wb.ru/exactmatch/ru/common/v4/search?" \
-                        "appType=1&couponsGeo=2,12,7,3,6,18,21&curr=rub&dest=-1029256,-81994,-912503,-941037&emp=0&lang=ru&locale=ru&page={page}&pricemarginCoeff=1.0&query={query}&reg=0&regions=68,64,83,4,38,80,33,70,82,86,30,69,22,66,31,40,1,48&resultset=catalog&sort=popular&spp=0"
-
-template_product_description_url = "https://wbx-content-v2.wbstatic.net/ru/{idx}.json"
-
-
 class WildParser:
-    def __init__(self):
-        self._data = []
-
     async def _collect_from_page(self, session, page, query):
         data = []
         json_ = await _utils.get_products(query, page, session)
@@ -40,7 +23,7 @@ class WildParser:
                 continue
             str_description = _(product_description.get("description", ""))
             photo_count = product_description.get("media", {"photo_count": None}).get('photo_count')
-            image_url = _utils.get_image_url(idx)
+            image_urls = _utils.get_image_url(idx, photo_count)
             name = _(product_description.get('imt_name') or products[i].get("name"))
             prev_price = products[i]["priceU"] // 100
             price_now = products[i]["salePriceU"] // 100
@@ -49,7 +32,7 @@ class WildParser:
 
             data.append({
                 "name": name,
-                "image": image_url,
+                "images": image_urls,
                 "prev_price": prev_price,
                 "price": price_now,
                 "price_diff": diff_price,
@@ -71,7 +54,7 @@ class WildParser:
                 if not data_from_page:
                     break
                 data += data_from_page
-
+        await session.close()
         return data
 
     async def _create_tasks(self, queries):
@@ -90,28 +73,8 @@ class WildParser:
         return loop.run_until_complete(self._create_tasks(queries))
 
 
-
-def _save_json(data, path: str) -> str:
-    s = _(json.dumps(data, indent=4, ensure_ascii=False))
-    with open(path, "w", encoding="cp1251") as f:
-        f.write(s)
-    return path
-
-
-# async def _create_tasks(queries):
-#     tasks = []
-#
-#     for el in queries:
-#         task = asyncio.create_task(collect_and_save(el))
-#         tasks.append(task)
-#     return await asyncio.gather(*tasks)
-
 if __name__ == '__main__':
     s_t = time.time()
-    # loop = asyncio.new_event_loop()
-    # asyncio.set_event_loop(loop)
-    # res = loop.run_until_complete(_create_tasks(["iphone"]))
     res = WildParser().collect("iphone", "macbook")
     print(round(time.time() - s_t, 5))
-    _save_json(res, "test_source/test.json")
 
